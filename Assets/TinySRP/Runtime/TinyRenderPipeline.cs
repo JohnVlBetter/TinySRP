ccc25ba12571423c064b64dc4080033d7d78ce94
JohnLee <lijiang01001@163.com>
2023-07-15 18:07:51 +0800

    editor only

--- Assets/TinySRP/Runtime/TinyRenderPipeline.cs
+++ Assets/TinySRP/Runtime/TinyRenderPipeline.cs
@@ -1,3 +1,4 @@
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.Rendering;
 
@@ -15,6 +16,11 @@ public class TinyRenderPipeline : RenderPipeline
 
     void RenderSingleCamera(ScriptableRenderContext context, Camera camera)
     {
+#if UNITY_EDITOR
+        //Scene显示ui
+        PrepareForSceneWindow(camera);
+#endif
+
         ScriptableCullingParameters cullingParameters;
         if (!camera.TryGetCullingParameters(out cullingParameters))
         {
@@ -25,18 +31,19 @@ public class TinyRenderPipeline : RenderPipeline
 
         context.SetupCameraProperties(camera);
 
-        var cmdBuffer = new CommandBuffer { name = camera.name };
+        var cmdBuffer = CommandBufferPool.Get(camera.name);
 
         var clearFlags = camera.clearFlags;
         //clear depth color
         cmdBuffer.ClearRenderTarget(
-            (clearFlags & CameraClearFlags.Depth) != 0, 
-            (clearFlags & CameraClearFlags.Color) != 0, 
-            camera.backgroundColor
+            clearFlags <= CameraClearFlags.Depth, 
+            clearFlags == CameraClearFlags.Color,
+            clearFlags == CameraClearFlags.Color ?
+                camera.backgroundColor.linear : Color.clear
         );
-        context.ExecuteCommandBuffer( cmdBuffer );
+        context.ExecuteCommandBuffer(cmdBuffer);
 
-        cmdBuffer.Release();
+        CommandBufferPool.Release(cmdBuffer);
 
         //绘制Opaque物体
         var sortingSettings = new SortingSettings(camera)
@@ -55,6 +62,69 @@ public class TinyRenderPipeline : RenderPipeline
         filteringSettings.renderQueueRange = RenderQueueRange.transparent;
         context.DrawRenderers(cullingResults, ref drawingSettings, ref filteringSettings);
 
+#if UNITY_EDITOR
+        //绘制不支持的材质
+        DrawUnsupportedShaders(context, camera, cullingResults);
+        //绘制Gizmos
+        DrawGizmos(context, camera);
+#endif
+
         context.Submit();
     }
+
+#region Editor Only
+#if UNITY_EDITOR
+    static ShaderTagId[] legacyShaderTagIds = {
+        new ShaderTagId("Always"),
+        new ShaderTagId("ForwardBase"),
+        new ShaderTagId("PrepassBase"),
+        new ShaderTagId("Vertex"),
+        new ShaderTagId("VertexLMRGBM"),
+        new ShaderTagId("VertexLM"),
+        new ShaderTagId("UniversalForward"),
+    };
+    static Material errorMaterial;
+    //绘制unity自带的shader，显示为洋红
+    void DrawUnsupportedShaders(ScriptableRenderContext context, Camera camera, CullingResults cullingResults)
+    {
+        if (errorMaterial == null)
+        {
+            errorMaterial = new Material(Shader.Find("Hidden/InternalErrorShader"));
+        }
+        var drawingSettings = new DrawingSettings(
+            legacyShaderTagIds[0], new SortingSettings(camera)
+        )
+        {
+            overrideMaterial = errorMaterial
+        }; ;
+        for (int i = 1; i < legacyShaderTagIds.Length; i++)
+        {
+            drawingSettings.SetShaderPassName(i, legacyShaderTagIds[i]);
+        }
+        var filteringSettings = FilteringSettings.defaultValue;
+        context.DrawRenderers(
+            cullingResults, ref drawingSettings, ref filteringSettings
+        );
+    }
+
+    //绘制Gizmos
+    void DrawGizmos(ScriptableRenderContext context, Camera camera)
+    {
+        if (Handles.ShouldRenderGizmos())
+        {
+            context.DrawGizmos(camera, GizmoSubset.PreImageEffects);
+            context.DrawGizmos(camera, GizmoSubset.PostImageEffects);
+        }
+    }
+
+    //Scene显示ui
+    void PrepareForSceneWindow(Camera camera)
+    {
+        if (camera.cameraType == CameraType.SceneView)
+        {
+            ScriptableRenderContext.EmitWorldGeometryForSceneView(camera);
+        }
+    }
+#endif
+    #endregion
 }
