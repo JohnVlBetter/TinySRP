b4b554fe31892dc656fb3b8221ad44cd95f1bddd
JohnLee <lijiang01001@163.com>
2023-07-15 17:17:41 +0800

    Draw renderers: Opaque and Transparent

--- Assets/TinySRP/Runtime/TinyRenderPipeline.cs
+++ Assets/TinySRP/Runtime/TinyRenderPipeline.cs
@@ -3,6 +3,8 @@ using UnityEngine.Rendering;
 
 public class TinyRenderPipeline : RenderPipeline
 {
+    static ShaderTagId unlitShaderTagId = new ShaderTagId("SRPDefaultUnlit");
+
     protected override void Render(ScriptableRenderContext context, Camera[] cameras)
     {
         foreach (var camera in cameras)
@@ -13,9 +15,45 @@ public class TinyRenderPipeline : RenderPipeline
 
     void RenderSingleCamera(ScriptableRenderContext context, Camera camera)
     {
+        ScriptableCullingParameters cullingParameters;
+        if (!camera.TryGetCullingParameters(out cullingParameters))
+        {
+            //没什么可渲染的就return
+            return;
+        }
+        CullingResults cullingResults = context.Cull(ref cullingParameters);
+
         context.SetupCameraProperties(camera);
 
+        var cmdBuffer = new CommandBuffer { name = camera.name };
+
+        var clearFlags = camera.clearFlags;
+        //clear depth color
+        cmdBuffer.ClearRenderTarget(
+            (clearFlags & CameraClearFlags.Depth) != 0, 
+            (clearFlags & CameraClearFlags.Color) != 0, 
+            camera.backgroundColor
+        );
+        context.ExecuteCommandBuffer( cmdBuffer );
+
+        cmdBuffer.Release();
+
+        //绘制Opaque物体
+        var sortingSettings = new SortingSettings(camera)
+        {
+            criteria = SortingCriteria.CommonOpaque
+        };
+        var drawingSettings = new DrawingSettings(unlitShaderTagId, sortingSettings);
+        var filteringSettings = new FilteringSettings(RenderQueueRange.opaque);
+        context.DrawRenderers(cullingResults, ref drawingSettings, ref filteringSettings);
+
+        //绘制天空盒
         context.DrawSkybox(camera);
+
+        //绘制Transparent物体
+        sortingSettings.criteria = SortingCriteria.CommonTransparent;
+        filteringSettings.renderQueueRange = RenderQueueRange.transparent;
+        context.DrawRenderers(cullingResults, ref drawingSettings, ref filteringSettings);
 
         context.Submit();
     }
